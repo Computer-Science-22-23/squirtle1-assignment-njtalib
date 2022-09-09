933be5149679381bc7ae67241b04e3efff04c8b9
ntjalib <njtalib@gmail.com>
2022-09-09 12:54:22 -0700

    updating code

--- Squirtle/src/BulbasaurThing.java
+++ Squirtle/src/BulbasaurThing.java
@@ -40,4 +40,65 @@ public class BulbasaurThing extends PokeThing {
 		Gui g = getBoard().getGui();
 		g.appendTextWindow("Bulbasaur has been tickled.");
 	}
+	
+	public void move() {
+		boolean blocked;
+		boolean stuck;
+		Gui g = getBoard().getGui();
+		// If this PokeThing is not currently in a Board, do nothing
+		if (null == getBoard())
+		{
+			return;
+		}
+		
+		// Check the 'next' Location.  If there is a PokeThing there, do not move.
+		
+		Location nextLoc = dir.getNextLocation(getLocation()); 
+		blocked = getBoard().thingAt(nextLoc) instanceof PokeThing;
+		stuck = !(nextLoc.isValid(getBoard()));
+		
+		if (getBoard().thingAt(nextLoc) instanceof PokeThing)
+		{
+			return;	
+		}
+		else if (stuck){
+			setDirection(getDirection().right());
+//			nextLoc = dir.getNextLocation(getLocation()); 
+//			System.out.println("DEBUG");
+		}
+		else {
+			setDirection(getDirection().left());
+			nextLoc = dir.getNextLocation(getLocation());
+			if (getBoard().thingAt(nextLoc) instanceof SquirtleThing)
+			{
+				setDirection(getDirection().right());
+				setDirection(getDirection().right());
+				nextLoc = dir.getNextLocation(getLocation());
+				
+			}
+			else {
+				setDirection(getDirection().right());
+				nextLoc = dir.getNextLocation(getLocation());
+			}
+//			move();
+			
+		}
+//		Location nextLoc = dir.getNextLocation(getLocation()); 
+		
+		super.move();
+		
+	}
+//	public void move(Location loc)
+//	{
+//		// If this PokeThing isn't in a board, or the location is valid, don't move
+//		if ((null == getBoard()) || (false == loc.isValid(getBoard())))
+//		{
+//			return;
+//		}
+//
+//		// Remove self from the current location on the Board and add to loc
+//		getBoard().remove(this);
+//		setLocation(loc);
+//		getBoard().add(this);
+//	}
 }
--- Squirtle/src/SquirtleThing.java
+++ Squirtle/src/SquirtleThing.java
@@ -7,7 +7,7 @@ public class SquirtleThing extends PokeThing {
 	 * SquirtleThing Constructor
 	 * Creates a new <code>SquirtleThing</code> object.
 	 * Associated graphics file will be squirtle.png
-	 ****/
+	 */
 	public SquirtleThing()
 	{
 		super("Squirtle", "", 0);	
@@ -24,10 +24,33 @@ public class SquirtleThing extends PokeThing {
 		movementPattern();
 		
 		Gui g = getBoard().getGui();
+		squaresMoved += 1;
 		g.appendTextWindow("Squirtle has moved " + squaresMoved + " squares.");
+		Location nextLoc = getDirection().getNextLocation(getLocation());
+
 	}
 	
-	
+	public void getFlower(Location loc) {
+		
+		
+		Board b = getBoard();
+		// Check the 'next' Location.  If there is a PokeThing there, do not move. 
+		if (getBoard().thingAt(loc) instanceof FlowerThing)
+		{
+			FlowerThing f = (FlowerThing) b.thingAt(loc);
+			b.remove(f);
+			
+			
+		}
+	/*	setLocation(0,0);
+		getBoard().remove(nextLoc);
+//		getBoard().remove(this);
+//		setLocation(1, 1);
+//		getBoard().add(this);
+		System.out.println(nextLoc);
+		move(nextLoc); */
+		
+	}
 	/**
 	 * movement in a repeating pattern
 	 * <code>SquirtleThing</code> turns right if it is blocked or encounters a wall
@@ -37,15 +60,30 @@ public class SquirtleThing extends PokeThing {
 		// Check the 'next' Location.  If there is a PokeThing or wall there, turn.
 		boolean blocked;
 		boolean stuck;
-			
+		boolean flowerBlock;
+		Gui g = getBoard().getGui();
+		
 		Location nextLoc = getDirection().getNextLocation(getLocation()); 
 		blocked = getBoard().thingAt(nextLoc) instanceof PokeThing;
+		
+		flowerBlock = getBoard().thingAt(nextLoc) instanceof FlowerThing;
+		
 		stuck = !(nextLoc.isValid(getBoard()));
-		if (blocked || stuck)
+		
+		if (stuck)
 		{
 			setDirection(getDirection().right());
+			nextLoc = getDirection().getNextLocation(getLocation()); 
 		}
-				
-		move();
+		else if (flowerBlock) {
+			getFlower(nextLoc);
+		}
+		else if (blocked) {
+			
+		}
+			move();
 	}
+
 }
+
+
